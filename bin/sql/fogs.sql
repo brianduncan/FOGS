2d9a97e31686468f28d29b0584a3057ccedca2ab
michael <themichaelyu@gmail.com>
2012-10-23 18:29:18 -0700

    added product table

--- bin/sql/fogs.sql
+++ bin/sql/fogs.sql
@@ -1,12 +1,22 @@
---<ScriptOptions statementTerminator=";"/>
+CREATE TABLE company 
+(
+facebook_id int NOT NULL, 
+name varchar(255) NOT NULL, 
+CONSTRAINT pk_company_facebook_id PRIMARY KEY (facebook_id)
+);
 
---CREATE TABLE company 
---(
---facebook_id int NOT NULL, 
---name varchar(255) NOT NULL, 
---CONSTRAINT pk_company_facebook_id PRIMARY KEY (facebook_id)
---);
+CREATE TABLE product
+(
+facebook_id int NOT NULL,
+name varchar(255) NOT NULL,
+company_id int NOT NULL,
+CONSTRAINT pk_product_facebook_id PRIMARY KEY (facebook_id)
+);
 
---INSERT INTO company (facebook_id,name) VALUES (1,"test");
+ALTER TABLE product
+ADD CONSTRAINT fk_product_refs_company
+FOREIGN KEY (company_id) REFERENCES company(facebook_id) 
+ON DELETE CASCADE;
 
-SELECT * FROM company;
+
+
