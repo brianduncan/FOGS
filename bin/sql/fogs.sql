19a092b03f8207b5b65f8baedc0f2c7866736a09
michael <themichaelyu@gmail.com>
2012-10-23 18:04:48 -0700

    initial commit - sql file for all initial sqls to create tables

---
+++ bin/sql/fogs.sql
@@ -0,0 +1,12 @@
+--<ScriptOptions statementTerminator=";"/>
+
+--CREATE TABLE company 
+--(
+--facebook_id int NOT NULL, 
+--name varchar(255) NOT NULL, 
+--CONSTRAINT pk_company_facebook_id PRIMARY KEY (facebook_id)
+--);
+
+--INSERT INTO company (facebook_id,name) VALUES (1,"test");
+
+SELECT * FROM company;
