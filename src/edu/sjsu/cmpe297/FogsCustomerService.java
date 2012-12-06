ef6bc21afa642006c26f23c611a8fb16337a3fae
Rahul Priyadarshi <rahulpriyadarshi@yahoo.com>
2012-12-05 20:49:59 -0800

    New changes to the code.

--- src/edu/sjsu/cmpe297/FogsCustomerService.java
+++ src/edu/sjsu/cmpe297/FogsCustomerService.java
@@ -146,6 +146,85 @@ public class FogsCustomerService {
 		  return retdata;
 	  }
 	  
+	  
+	  
+		//This method will return the number of likes for a product by the user's friends
+	  @GET
+	  @Path("/friendsview2/{userid}/{facebookprodid}") 
+	  @Produces(MediaType.APPLICATION_JSON)
+	  public String getFriendsVisitedProd2(@PathParam("userid") String userid, @PathParam("facebookprodid") String facebookprodid) 
+	  {
+		  String retdata = "";
+		  JSONObject j = new JSONObject();
+		  
+		  //Validate that all the fields have been passed
+		  if(StringUtils.isEmpty(userid) || StringUtils.isEmpty(facebookprodid)) 
+		  {
+			  j.put("101", "REQUIRED PARAMETER FIELDS MISSING");
+			  retdata = j.toString();	    	
+		  }
+		  else 
+		  {
+			  try 
+			  {
+				  //Get string of user's facebook friends
+				  OpenGraphUser openGraphUser = new OpenGraphUser(userid);
+				  openGraphUser.setAccessToken("AAAAAAITEghMBAAQrmHTYcfFta637ks4ZCgWuiVMuovxPXzvPcF5L3NRJtUpO6t11cxTfk2Bqb9kMZCEw00W3OIBot70ejhPJPAP1mCmCbaZCxfCqZCZAJ");
+				  String friends = openGraphUser.getFriendsString();
+				  
+				  //If friends returned for the user
+				  if(friends.toCharArray().length != 0)
+				  {
+					  JSONObject jusers = new JSONObject();
+
+					  //Get list of people who viewed the product
+					  ViewsDAO viewsDAO = ViewsDAO.getInstance();
+					  List<Views> productViewList = viewsDAO.getViewsForProduct(Long.parseLong(facebookprodid));
+					
+					  //Count for number of friends who viewed the product
+					  int count = 0;
+					  
+					  //Determine whether any of the user's friends viewed the product
+					  for(int i = 0; i < productViewList.size(); i++)
+					  {
+						  if(friends.contains(String.valueOf(productViewList.get(i).getUserId())))
+						  {
+							  count++;
+							  OpenGraphUser ogu = new OpenGraphUser(String.valueOf(productViewList.get(i).getUserId()));
+							  jusers.put(count, ogu.toJson());
+						  }
+					  }
+
+					  if(count > 0)
+					  {
+						  retdata = jusers.toString();
+					  }
+					  else
+					  {
+						  j.put("10", "NO FRIENDS FOUND");
+						  retdata = j.toString();
+					  }
+				  }
+				  else
+				  {
+					  j.put("10", "NO FRIENDS FOUND");
+					  retdata = j.toString();
+				  }
+			} 
+			catch (Exception e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				j.put("100", "ERROR RETRIEVING DATA");
+				retdata = j.toString();
+			}
+	    }
+		  
+		return retdata;
+	  }
+	  
+	  
+	  
 	  //This method will return the number of likes for a product by the user's friends
 	  @GET
 	  @Path("/friendsliked/{userid}/{facebookprodid}") 
